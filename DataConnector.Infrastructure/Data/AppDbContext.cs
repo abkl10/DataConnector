9956d333ca5f3c73542e168a158e8773034e5db2
Abderrahman Abaakil <abderrahmanebkl48@gmail.com>
2025-08-17 19:07:01 +0200

    update the appdbcontext to inherit from teh identitydbcontext

--- DataConnector.Infrastructure/Data/AppDbContext.cs
+++ DataConnector.Infrastructure/Data/AppDbContext.cs
@@ -1,24 +1,19 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using DataConnector.Infrastructure.Entities;
 using DataConnector.Core.Entities;
 
 namespace DataConnector.Infrastructure.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users => Set<User>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.ToTable("Users");
-            entity.HasKey(u => u.Id);
-        });
+        modelBuilder.Entity<User>().ToTable("Users");
     }
 }
